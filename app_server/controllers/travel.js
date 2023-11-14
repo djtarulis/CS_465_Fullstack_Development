5e39f61884600fe7d77a7ceb538d86a2cafd08b6
Daniel <djtarulis@gmail.com>
2023-11-13 18:59:08 -0600

    Express website, static HTML only

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -7,6 +7,7 @@ const travel = (req, res) => {
     console.log('Inside app_server, controllers, travel.js, travel function.');
     res.render('travel', { title: 'Travlr Getaways' });
     };
+    
 module.exports = {
     travel
 };
