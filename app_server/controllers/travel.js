9841ddac52591d2903b736c90d15d7f049680974
Daniel <djtarulis@gmail.com>
2023-11-13 19:02:27 -0600

    Express website, HTML only

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -5,9 +5,9 @@ var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
 /* GET travel view */
 const travel = (req, res) => {
     console.log('Inside app_server, controllers, travel.js, travel function.');
-    res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', { title: 'Travlr Getaways', trips });
     };
-    
+
 module.exports = {
     travel
 };
